dce09d33eba5a3c802162d0f3e6add38101f63cc
EChachati <chachati28@gmail.com>
2023-08-04 20:55:28 -0400

    Create Server

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+// Imports
 const { connection } = require("./database/connection");
+const express = require("express");
+const cors = require("cors");
 
 // innit app
 console.log("Running App");
 
 // connect to DB
 connection();
+
+// Create server Node
+const app = express();
+const port = 3900;
+
+//The next ones are Middleware
+app.use(cors()); // Configure CORS
+app.use(express.json()); // Convert body to JSON
+
+// Create Server and listen to HTTP requests
+app.listen(port, () => {
+  console.log("Server running on Port: " + port);
+});
