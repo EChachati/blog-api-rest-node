3e4f1fbf6b154c30a038c8c676f3c315942b66fa
EChachati <chachati28@gmail.com>
2023-08-04 20:41:50 -0400

    format

--- database/connection.js
+++ database/connection.js
@@ -1,16 +1,16 @@
- const mongoose = require("mongoose") // Equivalent to Import
+const mongoose = require("mongoose"); // Equivalent to Import
 
- const connection = async() => {
-    try{
-        await mongoose.connect("mongodb://127.0.0.1:27017/my_blog")
-        console.log("Good Connection!")
-    } catch(error){
-        console.log(error)
-        throw new Error("Unable connection to DB")
-    }
- }
+const connection = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/my_blog");
+    console.log("Good Connection!");
+  } catch (error) {
+    console.log(error);
+    throw new Error("Unable connection to DB");
+  }
+};
 
- // Export function to be use in other files
- module.exports = {
-    connection
- }
+// Export function to be use in other files
+module.exports = {
+  connection,
+};
