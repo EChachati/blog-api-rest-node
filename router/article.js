53889ec7543526adf5766820457c2cb8346329f3
EChachati <chachati28@gmail.com>
2023-08-05 16:49:06 -0400

    create middleware  load images

--- router/article.js
+++ router/article.js
@@ -1,9 +1,23 @@
 // Imports
 const express = require("express");
 const ArticleController = require("../controllers/article");
+const multer = require("multer");
 
 // Create router object
 const router = express.Router();
+
+// Create Storage
+const storage = multer.diskStorage({
+  destination: (request, file, cb) => {
+    cb(null, "./images/articles/");
+  },
+  filename: (request, file, cb) => {
+    cb(null, "article_" + Date.now() + file.originalname);
+  },
+});
+
+// Middleware
+const uploads = multer({ storage: storage });
 
 // Routes
 router.get("/test", ArticleController.test);
@@ -12,6 +26,11 @@ router.get("/list/:limit?", ArticleController.list);
 router.get("/get/:id", ArticleController.getById);
 router.delete("/delete/:id", ArticleController.deleteById);
 router.put("/update/:id", ArticleController.updateById);
+router.post(
+  "/load-image/:id",
+  [uploads.single("file")],
+  ArticleController.loadImage
+);
 
 // Export Router
 module.exports = router;
