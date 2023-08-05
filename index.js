5811fb597cf8c4831a50f164efe1d26d6a2b6eee
EChachati <chachati28@gmail.com>
2023-08-04 21:09:48 -0400

    Create a Route

--- index.js
+++ index.js
@@ -13,9 +13,18 @@ connection();
 const app = express();
 const port = 3900;
 
-//The next ones are Middleware
+// The next ones are Middleware
 app.use(cors()); // Configure CORS
 app.use(express.json()); // Convert body to JSON
+
+// Routes
+app.get("/", (request, response) => {
+  console.log("Called Endpoint test :3");
+  return response.status(200).json({
+    status: 200,
+    message: "Hello World from NodeJS",
+  });
+});
 
 // Create Server and listen to HTTP requests
 app.listen(port, () => {
